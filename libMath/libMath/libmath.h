838c0bd932ba140597b12b85ce52ce4e2f5a3e97
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2021-05-15 22:48:46 -0500

    Main header information.

--- libMath/libMath/libmath.h
+++ libMath/libMath/libmath.h
@@ -1,1 +1,4 @@
 #pragma once
+
+// File to be included ONLY if you are sure you want to include every header in this library.
+// If you only require a few headers, feel free to manually include only the headers you need.
